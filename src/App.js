106cca0131af8e07c73342cf44ab25cd3055fcee
Fernanda <acquaentertainment@gmail.com>
2020-06-19 06:16:47 -0700

    comprar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Producto from './components/Producto';
+import Carrito from './components/Carrito';
 function App() {
 
   // Crear listado de productos
@@ -12,6 +13,8 @@ function App() {
     { id: 4, nombre: 'Camisa AngularJS', precio:50 },
   ]);
 
+  //state para un carrito de compras
+  const [ carrito, agregarProducto ] = useState([]);
   // Obtener la fecha
   const fecha = new Date().getFullYear();
 
@@ -24,10 +27,17 @@ function App() {
         <h1>Lista de productos</h1>
         {productos.map(producto => (
           <Producto 
-            key={producto.id}
-            producto={producto}
+              key={producto.id}
+              producto={producto}
+              productos={productos}
+              carrito={carrito}
+              agregarProducto={agregarProducto}
           />
         ))}
+
+        <Carrito 
+          carrito={carrito}
+        />
 
         <Footer 
           fecha={fecha}
